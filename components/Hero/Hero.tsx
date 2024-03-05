ea760b4a4b50b8d8065ed57f2e22e02480d0f9ec
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-05 10:41:19 -0800

    UPDATED

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,12 +7,12 @@ const Hero = () => {
         <section className='w-full h-[540px] bg-gradient-to-r from-[#114B9B] to-[#002457] relative'>
             <div className='w-full relative'>
                 <div className='w-full absolute left-[240px] top-[28px] z-0'>
-                    <div className='w-[201px] h-[51px] relative '>
+                    <div className='w-[150px] h-[35px] relative '>
                         <Image src={LogoImg} alt='dentfix' fill />
                     </div>
                 </div>
-                <div className='w-full absolute z-0 border-2 border-red-600'>
-                    <div className='w-[433px] h-[517px] absolute top-0 right-0'>
+                <div className='w-full absolute z-0'>
+                    <div className='w-[300px] absolute top-[47px] right-[79px]'>
                         <Image src={ThoothImg} alt='dentfix' />
                     </div>
                     
