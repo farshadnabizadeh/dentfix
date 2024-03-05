a626417d1a0c503ac1f7c48094844cce4d061387
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-05 11:59:11 -0800

    updated

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -15,7 +15,14 @@ const Hero = () => {
                     <div className='w-[300px] absolute top-[47px] right-[79px]'>
                         <Image src={ThoothImg} alt='dentfix' />
                     </div>
-                    
+                </div>
+                <div className='w-full absolute z-0'>
+                    <div className='absolute top-[244px] left-[240px]'>
+                        <div className='text-white text-[35px] font-semibold'>Boost Your Confidence with</div>
+                        <div className='text-white text-[40px] font-bold'>A Hollywood Smile!</div>
+                        <div className='text-white text-[26px]'>We are dedicated to helping you <span className='font-bold'>achieve</span></div>
+                        <div className='text-white text-[26px]'>a beautiful smile that radiates <span className='font-bold'>confidence.</span></div>
+                    </div>
                 </div>
             </div>
         </section>
