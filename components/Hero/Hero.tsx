2555603a55de1bcae6021e7e3475faa85d150f4a
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-05 13:16:29 -0800

    UPDATED

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,15 +7,15 @@ import Girl from '@/assets/png/image.png';
 import HeroShape from '@/assets/png/HeroShape.png';
 const Hero = () => {
     return (
-        <section className='w-full h-[640px] bg-gradient-to-r from-[#114B9B] to-[#002457] relative overflow-hidden'>
+        <section className='w-full h-[900px] bg-gradient-to-r from-[#114B9B] to-[#002457] relative overflow-hidden'>
             <div className='w-full h-full absolute z-0'>
-                <div className='w-full absolute left-[240px] top-[28px] z-0'>
+                <div className='w-full absolute left-[240px] top-[48px] z-0'>
                     <div className='w-[150px] h-[35px] relative '>
                         <Image src={LogoImg} alt='dentfix' fill />
                     </div>
                 </div>
                 <div className='w-full absolute z-0'>
-                    <div className='w-[240px] absolute top-[47px] right-[79px]'>
+                    <div className='w-[320px] h-[417px] absolute top-[47px] right-[79px]'>
                         <Image src={ThoothImg} alt='dentfix' />
                     </div>
                 </div>
@@ -27,9 +27,9 @@ const Hero = () => {
                         <div className='text-white text-[26px]'>a beautiful smile that radiates <span className='font-bold'>confidence.</span></div>
                     </div>
                 </div>
-                <div className='w-[45%] h-[540px] absolute top-0 right-0 z-30'>
+                <div className='w-[800px] h-[540px] absolute top-[120px] right-0 z-30'>
                     <div className='w-full h-full flex justify-start items-center'>
-                        <div className='w-[70%] h-[80%] relative'>
+                        <div className='w-[601px] h-[586px] relative'>
                             <div className='w-full h-full absolute z-0 top-0 left-0'>
                                 <div className='w-full h-full relative'>
                                     <Image src={Girl} alt='Girl' fill />
@@ -39,7 +39,7 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-[110%] h-[500px] -left-[30px] -bottom-[20px] absolute z-30'>
+            <div className='w-[106%] h-[700px] -left-[30px] -bottom-[20px] absolute z-30'>
                 <div className='w-full h-[48px] left-[270px] top-[220px] absolute z-40'>
                     <div className='w-[330px] h-full bg-gradient-to-r from-[#FF9D17] to-[#FF7F00]  rounded-[24px] cursor-pointer'>
                         <div className='absolute w-[48px] h-[48px] bg-[#FF7F00] border-2 border-[#114B9B] rounded-full z-10'>
