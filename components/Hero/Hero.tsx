7c4d62070b208058518ccbd72dd113ce49de8617
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-05 13:01:45 -0800

    updated

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -15,12 +15,12 @@ const Hero = () => {
                     </div>
                 </div>
                 <div className='w-full absolute z-0'>
-                    <div className='w-[270px] absolute top-[47px] right-[79px]'>
+                    <div className='w-[240px] absolute top-[47px] right-[79px]'>
                         <Image src={ThoothImg} alt='dentfix' />
                     </div>
                 </div>
                 <div className='w-full absolute z-0'>
-                    <div className='absolute top-[170px] left-[240px]'>
+                    <div className='absolute top-[140px] left-[240px]'>
                         <div className='text-white text-[35px] font-semibold'>Boost Your Confidence with</div>
                         <div className='text-white text-[40px] font-bold'>A Hollywood Smile!</div>
                         <div className='text-white text-[26px]'>We are dedicated to helping you <span className='font-bold'>achieve</span></div>
@@ -40,7 +40,7 @@ const Hero = () => {
                 </div>
             </div>
             <div className='w-[110%] h-[500px] -left-[30px] -bottom-[20px] absolute z-30'>
-                <div className='w-full h-[48px] left-[270px] top-[240px] absolute z-40'>
+                <div className='w-full h-[48px] left-[270px] top-[220px] absolute z-40'>
                     <div className='w-[330px] h-full bg-gradient-to-r from-[#FF9D17] to-[#FF7F00]  rounded-[24px] cursor-pointer'>
                         <div className='absolute w-[48px] h-[48px] bg-[#FF7F00] border-2 border-[#114B9B] rounded-full z-10'>
                             <div className='w-full h-full flex items-center justify-center relative'>
@@ -54,7 +54,7 @@ const Hero = () => {
                         </div>
                     </div>
                 </div>
-                <div className='w-full h-full relative border-2 border-[red]'>
+                <div className='w-full h-full relative'>
                     <Image src={HeroShape} alt='Hero Shape' fill />
                 </div>
             </div>
