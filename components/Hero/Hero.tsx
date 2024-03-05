03419a6f2486e9d1f07508a05b16e2b2b9a3f2a4
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-05 12:17:20 -0800

    UPDATED

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -17,11 +17,17 @@ const Hero = () => {
                     </div>
                 </div>
                 <div className='w-full absolute z-0'>
-                    <div className='absolute top-[244px] left-[240px]'>
+                    <div className='absolute top-[170px] left-[240px]'>
                         <div className='text-white text-[35px] font-semibold'>Boost Your Confidence with</div>
                         <div className='text-white text-[40px] font-bold'>A Hollywood Smile!</div>
                         <div className='text-white text-[26px]'>We are dedicated to helping you <span className='font-bold'>achieve</span></div>
                         <div className='text-white text-[26px]'>a beautiful smile that radiates <span className='font-bold'>confidence.</span></div>
+                        <div className='w-full h-[48px] mt-[80px] relative'>
+                            <div className='w-[330px] h-full bg-gradient-to-r from-[#FF9D17] to-[#FF7F00]  rounded-[24px]'>
+                                <div className='absolute w-[48px] h-[48px] bg-[#FF7F00] border-2 border-[#114B9B] rounded-full'></div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
