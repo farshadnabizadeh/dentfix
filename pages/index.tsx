7e745f97a149ce9b3d0c522e11922a7735058427
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-05 15:05:11 -0800

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import useWindowDimensions from '@/hooks/useWindowDimensions';
 import Hero from '@/components/Hero/Hero';
+import ContactUs from '@/components/ContactUs/ContactUs';
 const Index = () => {
   const [isClient, setIsClient] = useState(false);
   const { height, width } = useWindowDimensions();
@@ -15,8 +16,19 @@ const Index = () => {
   }
 
   return (
-    <main className='w-full min-h-screen bg-white overflow-x-hidden'>
-      <Hero />
+    <main className='w-full min-h-screen bg-white overflow-x-hidden relative'>
+      <section className='w-full top-0 absolute z-0'>
+        <Hero />
+      </section>
+      <section className='w-full absolute z-10 top-[850px] '>
+        <div className='w-full flex justify-center'>
+          <div className='w-full flex justify-center'>
+            <div className='w-[1440px] border-2 border-[red]'>
+
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
