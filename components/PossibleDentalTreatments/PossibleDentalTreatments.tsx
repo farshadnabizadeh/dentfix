ad9a059e173647793302c45b69a111ddb56b9225
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 12:27:28 -0800

    UPDATED

--- components/PossibleDentalTreatments/PossibleDentalTreatments.tsx
+++ components/PossibleDentalTreatments/PossibleDentalTreatments.tsx
@@ -12,10 +12,10 @@ const PossibleDentalTreatments = () => {
                         <Image src={Shape2} alt='dentfix' fill />
                     </div>
                 </div>
-                <div className='w-full h-[733px] absolute z-10 top-[200px] px-[50px] 2xl:px-[240px] flex justify-center'>
+                <div className='w-full h-[733px] 2xl:h-[833px] absolute z-10 top-[200px] px-[50px] 2xl:px-[240px] flex justify-center'>
                     <div className='w-full h-full relative'>
                         <div className='w-full h-full absolute z-0 flex justify-end'>
-                            <div className='w-[610px] h-[633px] 2xl:w-[710px] 2xl:h-[733px] -translate-x-[100px] translate-y-4 2xl:translate-y-0 2xl:translate-x-0 relative'>
+                            <div className='w-[610px] h-[633px] 2xl:w-[710px] 2xl:h-[733px] -translate-x-[100px] translate-y-4 2xl:translate-y-[30px] 2xl:translate-x-0 relative'>
                                 <div className='w-full h-full  absolute z-0'>
                                     <Image src={GirlRightImage} alt='dentfix' fill />
                                 </div>
@@ -32,7 +32,7 @@ const PossibleDentalTreatments = () => {
                                 <div><span className='text-[#114B9B] text-[34px]'>in a</span><span className='text-[#30A1E8] mx-2 text-[34px] font-bold'>Hollywood Smile</span></div>
                             </div>
                         </div>
-                        <div className='w-[750px] 2xl:w-[647px] top-[100px] absolute z-40 border-2 border-[blue]'>
+                        <div className='w-[750px] 2xl:w-[780px] top-[100px] absolute z-40 border-2 border-[blue]'>
                             <div className='w-full h-[100px] 2xl:h-[130px] flex justify-start'>
                                 <div className='w-[650px] h-full border-2 border-[red]'>
 
