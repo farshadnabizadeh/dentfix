2794e981c8ac2db2cf8f2f329eebe1fcb57f898b
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 13:57:24 -0800

    UPDATED

--- components/HollywoodSmile/HollywoodSmile.tsx
+++ components/HollywoodSmile/HollywoodSmile.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const HollywoodSmile = () => {
-  return (
-    <div className='w-full flex justify-center'>
-      <div className='w-[1447px] h-[2034px]'>
-
-      </div>
-    </div>
-  );
+    return (
+        <div className='w-full flex justify-center'>
+            <div className='w-[1447px] h-[2034px]'>
+                <div className='w-full text-center text-[#114B9B] text-[34px] font-bold'>
+                    Hollywood Smile Packages with Zirconium
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default HollywoodSmile;
