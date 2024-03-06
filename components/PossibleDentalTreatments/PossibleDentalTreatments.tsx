e7bd14fedaad5206ea1c1377d4a865ebae29bde8
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 12:34:54 -0800

    UPDATED

--- components/PossibleDentalTreatments/PossibleDentalTreatments.tsx
+++ components/PossibleDentalTreatments/PossibleDentalTreatments.tsx
@@ -15,7 +15,7 @@ const PossibleDentalTreatments = () => {
                 <div className='w-full h-[733px] 2xl:h-[833px] absolute z-10 top-[200px] px-[50px] 2xl:px-[240px] flex justify-center'>
                     <div className='w-full h-full relative'>
                         <div className='w-full h-full absolute z-0 flex justify-end'>
-                            <div className='w-[610px] h-[633px] 2xl:w-[710px] 2xl:h-[733px] -translate-x-[100px] translate-y-4 2xl:translate-y-[30px] 2xl:translate-x-0 relative'>
+                            <div className='w-[610px] h-[633px] 2xl:w-[710px] 2xl:h-[733px] -translate-x-[100px] translate-y-[100px] 2xl:translate-y-[30px] 2xl:translate-x-0 relative'>
                                 <div className='w-full h-full  absolute z-0'>
                                     <Image src={GirlRightImage} alt='dentfix' fill />
                                 </div>
@@ -32,24 +32,24 @@ const PossibleDentalTreatments = () => {
                                 <div><span className='text-[#114B9B] text-[34px]'>in a</span><span className='text-[#30A1E8] mx-2 text-[34px] font-bold'>Hollywood Smile</span></div>
                             </div>
                         </div>
-                        <div className='w-[750px] mt-[50px] 2xl:mt-[30px] 2xl:w-[790px] top-[100px] absolute z-40 border-2 border-[blue]'>
+                        <div className='w-[750px] mt-[50px] 2xl:mt-[30px] 2xl:w-[790px] top-[100px] absolute z-40'>
                             <div className='w-full h-[100px] 2xl:h-[125px] flex justify-start'>
-                                <div className='w-[650px] h-full rounded-[10px] border-2 border-[red]'>
+                                <div className='w-[650px] h-full rounded-[10px] bg-[#EDF8FF]'>
 
                                 </div>
                             </div>
                             <div className='w-full h-[110px] mt-[38px] 2xl:h-[125px] flex justify-end'>
-                                <div className='w-[650px] h-full rounded-[10px] border-2  border-[red]'>
+                                <div className='w-[650px] h-full rounded-[10px] bg-[#EDF8FF] '>
 
                                 </div>
                             </div>
                             <div className='w-full h-[110px] mt-[38px] 2xl:h-[125px] flex justify-start'>
-                                <div className='w-[650px] h-full rounded-[10px] border-2 border-[red]'>
+                                <div className='w-[650px] h-full rounded-[10px] bg-[#EDF8FF]'>
 
                                 </div>
                             </div>
                             <div className='w-full h-[110px] mt-[38px] 2xl:h-[125px] flex justify-end'>
-                                <div className='w-[650px] h-full rounded-[10px] border-2  border-[red]'>
+                                <div className='w-[650px] h-full rounded-[10px] bg-[#EDF8FF] '>
 
                                 </div>
                             </div>
