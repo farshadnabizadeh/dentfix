211928387a52cf97407d74035be10c16aafa8fdc
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 14:03:09 -0800

    BodyVersionV9

--- components/HollywoodSmile/HollywoodSmile.tsx
+++ components/HollywoodSmile/HollywoodSmile.tsx
@@ -7,6 +7,23 @@ const HollywoodSmile = () => {
                 <div className='w-full text-center text-[#114B9B] text-[34px] font-bold'>
                     Hollywood Smile Packages with Zirconium
                 </div>
+                <div className='w-full grid grid-cols-3 gap-[100px]'>
+                    <div className='w-full mt-[38px]'>
+                        <div className='w-[350px] border-2 border-[red]'>
+
+                        </div>
+                    </div>
+                    <div className='w-full mt-[38px]'>
+                        <div className='w-[350px] border-2 border-[red]'>
+
+                        </div>
+                    </div>
+                    <div className='w-full mt-[38px]'>
+                        <div className='w-[350px] border-2 border-[red]'>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
