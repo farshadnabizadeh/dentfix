fd0f558653b24a76eeb65e322365993d259bfd54
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 13:55:10 -0800

    UPDATED

--- components/HollywoodSmile/HollywoodSmile.tsx
+++ components/HollywoodSmile/HollywoodSmile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HollywoodSmile = () => {
   return (
-    <div className='w-full flex justify-center border-2 border-[red]'>
+    <div className='w-full flex justify-center'>
       <div className='w-[1447px] h-[2034px]'>
 
       </div>
