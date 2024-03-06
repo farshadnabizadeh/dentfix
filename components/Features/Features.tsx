764b5efdd22a89dfa43517dd54a143906efd82ed
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 01:08:57 -0800

    updated

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -4,13 +4,46 @@ const Features = () => {
     return (
         <div className='w-full h-full flex'>
             <div className='w-full h-full flex-1 flex justify-center items-center pr-[28px]'>
-                <div className='text-[70px] text-[rgba(41,131,255,0.3)] font-bold'>01</div>
+                <div className='w-[300px]'>
+                    <div className='text-[70px] text-[rgba(41,131,255,0.3)] font-bold'>01</div>
+                    <div>
+                        <span className='text-[#114B9B] text-[30px] font-bold'>Online</span>
+                        <span className='text-[#30A1E8] text-[30px] font-bold ml-2'>Consultation</span>
+                    </div>
+                    <div>
+                        <span className='text-[#535353] text-[12px]'>
+                            For your online consultation we will ask you to provide your medical records and to submit photos of your teeth taken at specific angles. Alongside, we will discuss your smile goals and address any concerns you may have.
+                        </span>
+                    </div>
+                </div>
             </div>
             <div className='w-full h-full flex-1 flex justify-center items-center pr-[28px]'>
-                <div className='text-[70px] text-[rgba(41,131,255,0.6)] font-bold'>02</div>
+                <div className='w-[300px]'>
+                    <div className='text-[70px] text-[rgba(41,131,255,0.3)] font-bold'>01</div>
+                    <div>
+                        <span className='text-[#114B9B] text-[30px] font-bold'>Online</span>
+                        <span className='text-[#30A1E8] text-[30px] font-bold ml-2'>Consultation</span>
+                    </div>
+                    <div>
+                        <span className='text-[#535353] text-[12px]'>
+                            For your online consultation we will ask you to provide your medical records and to submit photos of your teeth taken at specific angles. Alongside, we will discuss your smile goals and address any concerns you may have.
+                        </span>
+                    </div>
+                </div>
             </div>
             <div className='w-full h-full flex-1 flex justify-center items-center pr-[28px]'>
-                <div className='text-[70px] text-[rgba(41,131,255,1)] font-bold'>03</div>
+                <div className='w-[300px]'>
+                    <div className='text-[70px] text-[rgba(41,131,255,0.3)] font-bold'>01</div>
+                    <div>
+                        <span className='text-[#114B9B] text-[30px] font-bold'>Online</span>
+                        <span className='text-[#30A1E8] text-[30px] font-bold ml-2'>Consultation</span>
+                    </div>
+                    <div>
+                        <span className='text-[#535353] text-[12px]'>
+                            For your online consultation we will ask you to provide your medical records and to submit photos of your teeth taken at specific angles. Alongside, we will discuss your smile goals and address any concerns you may have.
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     );
