11513e81dd1ae79037491e3dd4d43dcfa7641119
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-05 23:40:45 -0800

    UPDATED

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -6,6 +6,7 @@ import ArrowToRight from '@/assets/png/Icon akar-arrow-right.png';
 import Girl from '@/assets/png/image.png';
 import HeroShape from '@/assets/png/HeroShape.png';
 import Rosette from '@/assets/png/rosette.png';
+import Button from '../Button/Button';
 const Hero = () => {
     return (
         <section className='w-full h-[900px] bg-gradient-to-r from-[#114B9B] to-[#002457] relative overflow-hidden'>
@@ -45,17 +46,8 @@ const Hero = () => {
             </div>
             <div className='w-[106%] h-[700px] -left-[30px] -bottom-[20px] absolute z-30'>
                 <div className='w-full h-[48px] left-[270px] top-[220px] absolute z-40'>
-                    <div className='w-[330px] h-full bg-gradient-to-r from-[#FF9D17] to-[#FF7F00]  rounded-[24px] cursor-pointer'>
-                        <div className='absolute w-[48px] h-[48px] bg-[#FF7F00] border-2 border-[#114B9B] rounded-full z-10'>
-                            <div className='w-full h-full flex items-center justify-center relative'>
-                                <div className='w-[14px] h-[13px] relative'>
-                                    <Image src={ArrowToRight} alt='dentfix' fill />
-                                </div>
-                            </div>
-                        </div>
-                        <div className='w-[330px] h-full absolute z-0 flex justify-center items-center'>
-                            <span className='text-white font-bold text-[18px] translate-x-4'>Schedule Your Free Consult</span>
-                        </div>
+                    <div className='w-[330px] h-full relative'>
+                        <Button Title={'Schedule Your Free Consult'}/>
                     </div>
                 </div>
                 <div className='w-full h-full relative'>
