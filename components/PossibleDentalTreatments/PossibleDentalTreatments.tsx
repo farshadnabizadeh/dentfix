65f7acacbdaf35d31008419c7fa5fa4120f74804
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 11:34:44 -0800

    UPDATED

--- components/PossibleDentalTreatments/PossibleDentalTreatments.tsx
+++ components/PossibleDentalTreatments/PossibleDentalTreatments.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
-import Shape2 from '@/assets/png/shape2.png'
+import Shape2 from '@/assets/png/shape2.png';
+import GirlRightImage from '@/assets/png/girlRightImage.png';
+import Rosette from '@/assets/png/rosette.png';
 const PossibleDentalTreatments = () => {
     return (
         <>
@@ -8,6 +10,22 @@ const PossibleDentalTreatments = () => {
                 <div className='w-full h-[422px] absolute z-0'>
                     <div className='w-full h-full relative'>
                         <Image src={Shape2} alt='dentfix' fill />
+                    </div>
+                </div>
+                <div className='w-full h-[513px] absolute z-10 top-[200px] px-[200px] flex justify-center'>
+                    <div className='w-full h-full relative'>
+                        <div className='w-full h-full absolute z-0 flex justify-end'>
+                            <div className='w-[510px] h-full relative'>
+                                <div className='w-[510px] h-full absolute z-0'>
+                                    <Image src={GirlRightImage} alt='dentfix' fill />
+                                </div>
+                                <div className='w-[510px] h-full absolute z-10'>
+                                    <div className='absolute top-0 right-0 w-[180px] h-[190px] flex justify-center items-center'>
+                                        <Image src={Rosette} alt='dentfix' fill/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
