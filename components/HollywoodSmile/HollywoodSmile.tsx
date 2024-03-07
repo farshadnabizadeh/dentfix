3b01b2345807f291d8e505053c39bd19f6fa0e8a
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 18:50:34 -0800

    UPDATED

--- components/HollywoodSmile/HollywoodSmile.tsx
+++ components/HollywoodSmile/HollywoodSmile.tsx
@@ -12,7 +12,7 @@ const HollywoodSmile = () => {
                     <div className='w-full mt-[38px] relative'>
                         <div className='w-full h-full flex justify-center absolute top-0 z-10'>
                             <div className='w-[350px] h-full relative'>
-                                <div className='w-full h-[736px] relative'>
+                                <div className='w-full h-[736px] relative border-2 border-[red]'>
                                     <div className='w-full h-[597px] rounded-tl-[71px] rounded-br-[71px] rounded-tr-[12px] rounded-bl-[12px] bg-[#EDF5FF] absolute bottom-0 left-0 z-0'>
                                         <div className='w-full h-[168px] absolute -top-[70px] left-0 z-10 flex justify-center'>
                                             <div className='w-[168px] h-[168px] bg-gradient-to-b from-[#FCFDFF] to-[#D0E5FF] rounded-tr-[83px] rounded-bl-[83px] rounded-tl-[12px] rounded-br-[12px] flex justify-center items-center'>
@@ -30,7 +30,7 @@ const HollywoodSmile = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='w-full h-[900px] absolute z-30 top-0 border-2 border-[red]'>
+                        <div className='w-full h-[900px] absolute z-30 top-0'>
 
                         </div>
                         <div className='w-full absolute z-50 top-[240px]'>
