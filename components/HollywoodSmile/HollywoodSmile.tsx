76fc8bd167941d9374c46d214369b443f1dceaa5
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 18:15:44 -0800

    UPDATED

--- components/HollywoodSmile/HollywoodSmile.tsx
+++ components/HollywoodSmile/HollywoodSmile.tsx
@@ -11,21 +11,24 @@ const HollywoodSmile = () => {
                     <div className='w-full mt-[38px]'>
                         <div className='w-[350px] h-[736px] relative'>
                             <div className='w-full h-[597px] rounded-tl-[71px] rounded-br-[71px] rounded-tr-[12px] rounded-bl-[12px] bg-[#EDF5FF] absolute bottom-0 left-0 z-0'>
-
-                            </div>
-                            <div className='w-full h-[168px] absolute top-[70px] left-0 z-10 flex justify-center'>
-                                <div className='w-[168px] h-[168px] bg-gradient-to-b from-[#FCFDFF] to-[#D0E5FF] rounded-tr-[83px] rounded-bl-[83px] rounded-tl-[12px] rounded-br-[12px] flex justify-center items-center'>
-                                    <div className='w-full'>
-                                        <div className='w-full flex justify-center'>
-                                            <div className='text-[#3DA4FF] text-[39px] font-bold'>3.690€</div>
-                                        </div>
-                                        <div className='w-full flex justify-center'>
-                                            <div className='text-[#3DA4FF] text-[20px] -translate-y-[10px] font-semibold'>start from</div>
+                                <div className='w-full h-[168px] absolute -top-[70px] left-0 z-10 flex justify-center'>
+                                    <div className='w-[168px] h-[168px] bg-gradient-to-b from-[#FCFDFF] to-[#D0E5FF] rounded-tr-[83px] rounded-bl-[83px] rounded-tl-[12px] rounded-br-[12px] flex justify-center items-center'>
+                                        <div className='w-full'>
+                                            <div className='w-full flex justify-center'>
+                                                <div className='text-[#3DA4FF] text-[39px] font-bold'>3.690€</div>
+                                            </div>
+                                            <div className='w-full flex justify-center'>
+                                                <div className='text-[#3DA4FF] text-[20px] -translate-y-[10px] font-semibold'>start from</div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            <div className='w-full absolute top-[260px] z-10  border-2 border-[red]'>
+
+                            </div>
                         </div>
+
                         <div>welcome</div>
                     </div>
                     <div className='w-full mt-[38px]'>
