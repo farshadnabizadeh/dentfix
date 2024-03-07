73228272fc2bc95673ff9ed25820a3cbaa9cc183
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-06 18:37:27 -0800

    UPDATED

--- components/HollywoodSmile/HollywoodSmile.tsx
+++ components/HollywoodSmile/HollywoodSmile.tsx
@@ -8,28 +8,37 @@ const HollywoodSmile = () => {
                     Hollywood Smile Packages with Zirconium
                 </div>
                 <div className='w-full grid grid-cols-3 gap-[100px]'>
-                    <div className='w-full mt-[38px]'>
-                        <div className='w-[350px] h-[736px] relative'>
-                            <div className='w-full h-[597px] rounded-tl-[71px] rounded-br-[71px] rounded-tr-[12px] rounded-bl-[12px] bg-[#EDF5FF] absolute bottom-0 left-0 z-0'>
-                                <div className='w-full h-[168px] absolute -top-[70px] left-0 z-10 flex justify-center'>
-                                    <div className='w-[168px] h-[168px] bg-gradient-to-b from-[#FCFDFF] to-[#D0E5FF] rounded-tr-[83px] rounded-bl-[83px] rounded-tl-[12px] rounded-br-[12px] flex justify-center items-center'>
-                                        <div className='w-full'>
-                                            <div className='w-full flex justify-center'>
-                                                <div className='text-[#3DA4FF] text-[39px] font-bold'>3.690€</div>
-                                            </div>
-                                            <div className='w-full flex justify-center'>
-                                                <div className='text-[#3DA4FF] text-[20px] -translate-y-[10px] font-semibold'>start from</div>
+                    <div className='w-full mt-[38px] relative'>
+                        <div className='w-full h-full flex justify-center absolute top-0 z-10'>
+                            <div className='w-[350px] h-full relative'>
+                                <div className='w-full h-[736px] relative'>
+                                    <div className='w-full h-[597px] rounded-tl-[71px] rounded-br-[71px] rounded-tr-[12px] rounded-bl-[12px] bg-[#EDF5FF] absolute bottom-0 left-0 z-0'>
+                                        <div className='w-full h-[168px] absolute -top-[70px] left-0 z-10 flex justify-center'>
+                                            <div className='w-[168px] h-[168px] bg-gradient-to-b from-[#FCFDFF] to-[#D0E5FF] rounded-tr-[83px] rounded-bl-[83px] rounded-tl-[12px] rounded-br-[12px] flex justify-center items-center'>
+                                                <div className='w-full'>
+                                                    <div className='w-full flex justify-center'>
+                                                        <div className='text-[#3DA4FF] text-[39px] font-bold'>3.690€</div>
+                                                    </div>
+                                                    <div className='w-full flex justify-center'>
+                                                        <div className='text-[#3DA4FF] text-[20px] -translate-y-[10px] font-semibold'>start from</div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className='w-full absolute top-[260px] z-10  border-2 border-[red]'>
+                        </div>
+                        <div className='w-full h-[900px] absolute z-30 top-0 border-2 border-[red]'>
+                            <div className='w-full absolute bottom-0'>
+                                welcome
+                            </div>
+                        </div>
+                        <div className='w-full absolute z-20 top-[260px]'>
+                            <div className='w-full'>
 
                             </div>
                         </div>
-
-                        <div>welcome</div>
                     </div>
                     <div className='w-full mt-[38px]'>
                         <div className='w-[350px] border-2 border-[red]'>
